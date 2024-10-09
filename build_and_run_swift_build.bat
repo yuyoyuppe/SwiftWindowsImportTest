feaa94b060d9c22e556b11b3dedb10c2266b0d07
Andrey Nekrasov <1828123+yuyoyuppe@users.noreply.github.com>
2024-10-09 14:40:52 +0200

    do not use symlinks, because they're broken on Windows on Swift 6

--- build_and_run_swift_build.bat
+++ build_and_run_swift_build.bat
@@ -9,18 +9,18 @@ cd ..
 @REM Copy the module artifacts to the client app directories
 cd ClientApp
 mkdir .\deps\HelloModule\
-cp ..\HelloModule\.build\x86_64-unknown-windows-msvc\debug\HelloModule.swiftinterface .\deps\HelloModule\
-cp ..\HelloModule\.build\x86_64-unknown-windows-msvc\debug\HelloModule.private.swiftinterface .\deps\HelloModule\
 cp ..\HelloModule\.build\x86_64-unknown-windows-msvc\debug\HelloModule.lib .\deps\HelloModule\
 cp ..\HelloModule\.build\x86_64-unknown-windows-msvc\debug\HelloModule.dll .\deps\HelloModule\
+cp ..\HelloModule\.build\x86_64-unknown-windows-msvc\debug\Modules\HelloModule.swiftinterface .\deps\HelloModule\
+cp ..\HelloModule\.build\x86_64-unknown-windows-msvc\debug\Modules\HelloModule.private.swiftinterface .\deps\HelloModule\
 
 @REM Build the client app that uses the module with swift build
 swift build
 
 @REM Copy the .dll to the same directory as client .exe
-cp .\deps\HelloModule\HelloModule.dll .\.build\debug\
+cp .\deps\HelloModule\HelloModule.dll .\.build\x86_64-unknown-windows-msvc\debug\
 
 @REM Test it!
-.\.build\debug\ClientApp.exe
+.\.build\x86_64-unknown-windows-msvc\debug\ClientApp.exe
 
 cd ..
